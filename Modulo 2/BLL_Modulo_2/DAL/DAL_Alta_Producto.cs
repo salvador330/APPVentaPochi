b78907a798a5ef5d4cc6c1033034b161e4e69e0b
salvador330 <salvador.cirino@sita.aero>
2021-07-10 15:09:06 -0300

    cambios

---
+++ Modulo 2/BLL_Modulo_2/DAL/DAL_Alta_Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL
+{
+    public class DAL_Alta_Producto
+    {
+        //alta
+
+        //baja
+
+        //modificacion
+    }
+}
